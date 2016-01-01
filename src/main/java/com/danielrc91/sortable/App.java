1d6840b64f7ddbffc4d173e7640bf3dd4cf35d7f
Daniel Rodriguez <danielrodriguezcontreras91@gmail.com>
2016-01-01 16:18:15 +0100

    Setup Environment

---
+++ src/main/java/com/danielrc91/sortable/App.java
@@ -0,0 +1,13 @@
+package com.danielrc91.sortable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
